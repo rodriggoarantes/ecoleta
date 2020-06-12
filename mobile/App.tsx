c77dd43ac4c74a48f5776b75bc3bcb2555b6944f
Rodrigo Arantes <rodriggoarantes@gmail.com>
2020-06-12 18:59:07 -0300

    #nlw corrigindo configuraçao de rotaas

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
 
-import Routes from './src/routes';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Main from './src/pages/Main';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <>
       <StatusBar barStyle="light-content" />
-      <Routes />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Main" component={Main} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
